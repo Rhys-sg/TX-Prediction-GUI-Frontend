399520ae61b0c6533eba394323c212428fb949f5
Rhys Sorenson-Graff <sorensor@whitman.edu>
2024-09-02 23:28:51 -0700

    email passed

--- src/components/NewInput.vue
+++ src/components/NewInput.vue
@@ -201,13 +201,13 @@ export default {
     async insertObservedTX() {
       try {
         console.log(this.internalCurrentPromoterSequence);
-        console.log(this.email);
+        console.log(email);
         console.log(this.internalInputSets.map(entry => `${entry.email}: ${entry.firstname} ${entry.lastname}`).join(', '));
         console.log(this.internalInputObservedTX);
         console.log(this.internalInputNotes);
         const response = await axios.post(`${this.backendUrl}/insert_observed_TX`, {
           codingStrand: this.internalCurrentPromoterSequence,
-          account_email: this.email,
+          account_email: email,
           students: this.internalInputSets.map(entry => `${entry.email}: ${entry.firstname} ${entry.lastname}`).join(', '),
           observed_TX: this.internalInputObservedTX,
           notes: this.internalInputNotes,
