dc9b89e67b142eee72ae2ad9e948a06e6864496a
Rhys Sorenson-Graff <sorensor@whitman.edu>
2024-09-02 23:06:33 -0700

    sync NewInput to test

--- src/components/NewInput.vue
+++ src/components/NewInput.vue
@@ -3,26 +3,22 @@
     ref="newInputForm"
     @submit.prevent="newInputSubmit()"
   >
-  <v-card title="New Input" style="width: 800px; max-height: 625px; top: 50%; left: 50%; transform: translate(-50%, 0%);">
+    <v-card title="New Input" style="width: 800px; max-height: 625px; top: 50%; left: 50%; transform: translate(-50%, 0%);">
       <v-card-text style="padding-bottom: 0px;">
-                              
-        <!-- First Section: student(s) -->
         <h4 style="padding-bottom: 16px;">Students</h4>
-
-        <!-- Dynamically add row for each student entry -->
         <v-row v-for="(inputSet, index) in internalInputSets" :key="index">
 
-          <!-- Remove student -->
           <v-col cols="12" md="1" class="d-flex align-center">
             <v-btn
               icon="mdi-minus"
               size="small"
               style="transform: translate(0%, -12.5%);"
               @click="removeInputSet(index)"
+              :readonly="inputSets.length <= 1"
+              :variant="inputSets.length <= 1 ? 'plain' : 'elevated'"
             ></v-btn>
           </v-col>
 
-          <!-- First name -->
           <v-col cols="12" md="3" style="padding-bottom: 0px;">
             <v-text-field
               v-model="inputSet.firstname"
@@ -32,7 +28,6 @@
             ></v-text-field>
           </v-col>
 
-          <!-- Last name -->
           <v-col cols="12" md="4" style="padding-bottom: 0px;">
             <v-text-field
               v-model="inputSet.lastname"
@@ -42,7 +37,6 @@
             ></v-text-field>
           </v-col>
 
-          <!-- Email -->
           <v-col cols="12" md="4" style="padding-bottom: 0px;">
             <v-text-field
               v-model="inputSet.email"
@@ -54,7 +48,6 @@
           </v-col>
         </v-row>
 
-        <!-- Add new student -->
         <v-row>
           <v-col cols="12" md="1" class="d-flex align-center">
             <v-btn
@@ -72,7 +65,6 @@
           </v-container>
         </v-row>
 
-        <!-- Second section: promoter sequence and TX rate -->
         <v-row style="padding-bottom: 12px;">
           <v-col cols="12" md="8">
             <v-text-field
@@ -97,7 +89,6 @@
           </v-col>
         </v-row>
         
-        <!-- Third section: extra notes for input -->
         <v-textarea
           v-model="internalInputNotes"
           label="Notes"
@@ -109,7 +100,7 @@
 
       <v-card-actions style="padding-top: 0px; padding-bottom: 12px; padding-right: 22px;">
         <v-btn text="Close" variant="text" @click="$emit('close')">Close</v-btn>
-        <v-btn text="Submit" type="submit"></v-btn>
+        <v-btn text="Submit" type="submit" variant="tonal"></v-btn>
       </v-card-actions>
     </v-card>
   </v-form>
@@ -121,6 +112,10 @@ import axios from 'axios';
 export default {
   props: {
     backendUrl: {
+      type: String,
+      required: true
+    },
+    email: {
       type: String,
       required: true
     },
@@ -153,6 +148,7 @@ export default {
       emailRules: [
         v => !!v || 'Email is required',
         v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+        v => this.validateDomain(v) || 'E-mail must have a valid domain',
       ],
       codingStrandRules: [
         v => !!v || 'Coding strand is required',
@@ -160,6 +156,7 @@ export default {
       observedTXRateRules: [
         v => !!v || 'Observed TX Rate is required',
       ],
+      validDomains: [],
     };
   },
   watch: {
@@ -176,6 +173,9 @@ export default {
       this.$emit('update:internalInputNotes', newVal);
     },
   },
+  created() {
+    this.getValidDomain();
+  },
   methods: {
     addInputSet() {
       this.inputSets.push({
@@ -186,10 +186,9 @@ export default {
     },
 
     removeInputSet(index) {
-      if (this.inputSets.length <= 1) {
-        return;
+      if (this.inputSets.length > 0) {
+        this.inputSets.splice(index, 1);
       }
-      this.inputSets.splice(index, 1);
     },
 
     async newInputSubmit() {
@@ -200,20 +199,36 @@ export default {
       }
     },
     async insertObservedTX() {
-      console.log("test")
       try {
         const response = await axios.post(`${this.backendUrl}/insert_observed_TX`, {
-          students: this.inputSets,
-          codingStrand: this.currentPromoterSequence,
-          TX: this.inputObservedTX,
-          Notes: this.inputNotes,
+          codingStrand: this.internalCurrentPromoterSequence,
+          account_email: this.email,
+          students: this.internalInputSets.map(entry => `${entry.email}: ${entry.firstname} ${entry.lastname}`).join(', '),
+          observed_TX: this.internalInputObservedTX,
+          notes: this.internalInputNotes,
         })
         console.log(response.data.success);
-        console.log(response.data.error);
       } catch (error) {
         console.error('An error occurred.', error)
       }
     },
+
+    async getValidDomain() {
+      try {
+        const response = await axios.post(`${this.backendUrl}/get_valid_domain`);
+        this.validDomains = response.data.emails;
+      } catch (error) {
+        console.error('An error occurred.', error);
+      }
+    },
+
+    validateDomain(email) {
+      if (!this.validDomains.length) {
+        return false;
+      }
+      let domain = email.split('@')[1];
+      return this.validDomains.some(validDomain => domain.endsWith(validDomain));
+    }
   }
 };
 </script>
