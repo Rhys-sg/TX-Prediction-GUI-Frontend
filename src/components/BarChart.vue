aefa1aedd9737066d42f472a03ea38eab24d216f
Rhys Sorenson-Graff <sorensor@whitman.edu>
2025-04-08 11:27:24 -0700

    Update BarChart.vue

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -54,7 +54,7 @@ export default {
           },
           y: {
             type: 'logarithmic',
-            min: 50000,
+            min: 40000,
             max: this.maxY,
             ticks: {
               callback: function (value) {
